7eee23482203cb8ecb8f3fdcc9316b30e2175346
Maninderdeep Singh <93085866+Maninderr42@users.noreply.github.com>
2023-10-03 23:03:32 +0530

    pizza answer:

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,16 +3,16 @@ package com.driver;
 public class Main {
   public static void main(String[] args) {
     DeluxePizza dp = new DeluxePizza(true);
-    System.out.println(dp.getPrice());//n veg = 400+80+120 = 600  || 300+80+70= 450
+    System.out.println(dp.getPrice());
     dp.addTakeaway();
     dp.addExtraCheese();
-    dp.addExtraToppings();
-    System.out.println(dp.getBill()); //600+20+80+120||70
+    dp.addTakeaway();
+    System.out.println(dp.getBill());
 
-    Pizza p = new Pizza(false);
+    Pizza p = new Pizza(true);
     p.addExtraCheese();
     p.addExtraToppings();
-    // p.addTakeaway();
-    System.out.println(p.getBill());//400+80+120+20 = 620
+    p.addTakeaway();
+    System.out.println(p.getBill());
   }
 }
